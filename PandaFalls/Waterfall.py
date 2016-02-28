e0e2c218fb36073389c04c03c14246ab74802349
James Leung <James.Leung@capitalone.com>
2016-02-28 01:30:09 -0500

    Basic functionality

--- PandaFalls/Waterfall.py
+++ PandaFalls/Waterfall.py
@@ -1,10 +1,7 @@
-import pandas as pd
-
 class waterfall:
     def __init__(self, name, dataframe):
-        self.name = name
         self.dataframe = dataframe
-        self.equation = []
+        self.equation = [('=', name)]
 
     def plus(self, column):
         self.equation.append(('+', column))
@@ -13,3 +10,72 @@ class waterfall:
     def minus(self, column):
         self.equation.append(('-', column))
         return self
+
+    def to_c3(self):
+        data = self.generate_data()
+        data['type'] = 'bar'
+        data['order'] = """
+                        function(column1, column2){
+                        if (column2.id == 'Calculated'){
+                            return 1 } else {
+                            return 0
+                            }
+                        }
+                        """
+        return {
+            'data': data,
+            'grid': '{ y: { lines: [{value:0}] }}'
+        }
+
+    def equation_index(self, variable):
+        try:
+            index = self.equation.index(('+', variable))
+        except:
+            index = self.equation.index(('-', variable))
+        return index
+
+    def calculate_net(self):
+        net = 0
+        for variable in self.equation:
+            if variable[0] == '+':
+                net += self.dataframe[variable[1]][0]
+            if variable[0] == '-':
+                net -= self.dataframe[variable[1]][0]
+        return net
+
+    def generate_data(self):
+        data = dict(columns=[], groups=[])
+
+        for variable in self.equation:
+            column = variable[1]
+
+            values = [0] * (len(self.equation) + 1)
+            values[0] = column
+            if variable[0] == '=':
+                values[-1] = self.calculate_net()
+            else:
+                values[self.equation_index(column)] = self.dataframe[column][0]
+
+            data['columns'].append(values)
+            data['groups'].append(column)
+        data['columns'].append(self.calculated_column())
+
+        return data
+
+    def calculated_column(self):
+        calculated_column = [0] * (len(self.equation) + 1)
+        calculated_column[0] = 'Calculated'
+        for i in range(2,len(self.equation)):
+            operation = self.equation[i][0]
+            num_operation = -1 if operation == '-' else 1
+
+            name = self.equation[i][1]
+            value = self.dataframe[name][0]
+
+            name_prev = self.equation[i-1][1]
+            value_prev = self.dataframe[name_prev][0]
+
+            calculated_column[i] = value_prev + num_operation*value
+
+        return calculated_column
+
--- PandaFalls/tests/test_waterfall.py
+++ PandaFalls/tests/test_waterfall.py
@@ -1,65 +1,38 @@
 import unittest
 import pandas as pd
 from PandaFalls.Waterfall import waterfall
-from expects import equal, expect
+from expects import equal, expect, be_true
 
 
 class TestWaterfall(unittest.TestCase):
     def test_waterfall_can_add_column(self):
         df = pd.DataFrame({'Revenue': [500], 'Cost': [200]})
         profit = waterfall('Profit', df).plus('Revenue')
-        expect(profit.equation).to(equal([('+', 'Revenue')]))
-        # c3_json = """{
-        #     data: {
-        #         columns:[
-        #             ['Revenue', 500],
-        #             ['Calculated', 0],
-        #             ['Profit', 500]
-        #         ],
-        #         type:#'bar',
-        #         order: function(column1, column2){
-        #                 if (column2.id == 'Calculated'){
-        #                     return 1 } else {
-        #                     return 0
-        #                     }
-        #                 },
-        #         'groups': [
-        #             ['Revenue','Cost','Profit']
-        #             ]
-        #     },
-        #
-        #     'grid': { y: { lines: [{value:0}] }}
-        # }"""
-        # expect(profit.to_c3()).to(equal(c3_json))
+        expect(profit.equation).to(equal([('=', 'Profit'), ('+', 'Revenue')]))
 
     def test_waterfall_can_subtract_column(self):
         df = pd.DataFrame({'Revenue': [500], 'Cost': [200]})
         profit = waterfall('Profit', df).plus('Revenue').minus('Cost')
-        expect(profit.equation).to(equal([('+', 'Revenue'), ('-', 'Cost')]))
+        expect(profit.equation).to(equal([('=', 'Profit'), ('+', 'Revenue'), ('-', 'Cost')]))
 
-        # def test_waterfall_can_subtract_column(self):
-        #     df = pd.DataFrame({'Revenue': [500], 'Cost': [200]})
-        #     profit = Waterfall('Profit', df).plus('Revenue').minus('Cost')
-        #     c3_json = """{
-        #         data: {
-        #             columns:[
-        #                 ['Revenue', 500, 0, 0],
-        #                 ['Cost', 0, 300, 0],
-        #                 ['Calculated', 0, 200, 0],
-        #                 ['Profit', 0, 0, 300]
-        #             ],
-        #             type:#'bar',
-        #             order: function(column1, column2){
-        #                     if (column2.id == 'Calculated'){
-        #                         return 1 } else {
-        #                         return 0
-        #                         }
-        #                     },
-        #             'groups': [
-        #                 ['Revenue','Cost','Profit']
-        #                 ]
-        #         },
-        #
-        #         'grid': { y: { lines: [{value:0}] }}
-        #     }"""
-        #     expect(profit.to_c3()).to(equal(c3_json))
+    def test_waterfall_can_output_c3_json(self):
+        df = pd.DataFrame({'Revenue': [500], 'Cost': [200]})
+        profit = waterfall('Profit', df).plus('Revenue').minus('Cost')
+
+        c3_json = profit.to_c3()
+
+        expect(c3_json['data']['type']).to(equal('bar'))
+        expect(c3_json['data']['columns']).to(equal([
+            ['Profit', 0, 0, 300],
+            ['Revenue', 500, 0, 0],
+            ['Cost', 0, 200, 0],
+            ['Calculated', 0, 300, 0]
+        ]))
+
+        expect(self.checkEqual(
+            c3_json['data']['groups'],
+            ['Revenue', 'Cost', 'Profit']
+        )).to(be_true)
+
+    def checkEqual(self, list1, list2):
+        return len(list1) == len(list2) and sorted(list1) == sorted(list2)

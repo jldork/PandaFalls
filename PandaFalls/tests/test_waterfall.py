3912bd4040e19e20752e41eed33115c7b15c39ae
James Leung <James.Leung@capitalone.com>
2016-02-28 02:16:53 -0500

    Spacing for tests

--- PandaFalls/tests/test_waterfall.py
+++ PandaFalls/tests/test_waterfall.py
@@ -8,11 +8,13 @@ class TestWaterfall(unittest.TestCase):
     def test_waterfall_can_add_column(self):
         df = pd.DataFrame({'Revenue': [500], 'Cost': [200]})
         profit = waterfall('Profit', df).plus('Revenue')
+        
         expect(profit.equation).to(equal([('Revenue', 500)]))
 
     def test_waterfall_can_subtract_column(self):
         df = pd.DataFrame({'Revenue': [500], 'Cost': [200]})
         profit = waterfall('Profit', df).plus('Revenue').minus('Cost')
+
         expect(profit.equation).to(equal([('Revenue', 500),('Cost',-200)]))
 
     def test_waterfall_can_output_c3_json(self):
@@ -20,7 +22,7 @@ class TestWaterfall(unittest.TestCase):
         profit = waterfall('Profit', df).plus('Revenue').minus('Cost')
 
         c3_json = profit.to_c3()
-        print c3_json['columns']
+
         expect(self.checkEqual(c3_json['columns'], [
             ['Profit', 0, 0, 300],
             ['Revenue', 500, 0, 0],
